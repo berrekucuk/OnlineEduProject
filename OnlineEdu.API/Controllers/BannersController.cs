52e55ae1f02c3761af03770c7c02dbaebfadef5b
berrekucuk <berrekucuk@hotmail.com>
2024-12-14 18:12:08 +0300

    Add project files.

---
+++ OnlineEdu.API/Controllers/BannersController.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OnlineEdu.Business.Abstract;
+using OnlineEdu.Entity.Entities;
+
+namespace OnlineEdu.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BannersController(IGenericService<Banner> _bannerService,IMapper _mapper) : ControllerBase
+    {
+
+    }
+}
